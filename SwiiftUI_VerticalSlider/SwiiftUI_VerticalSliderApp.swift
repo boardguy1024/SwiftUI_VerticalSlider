ae72103e183f39a704b19ea8c0a364fcba20adbd
boardguy1024 <boardguy1024@gmail.com>
2022-09-04 14:52:30 +0900

    Initial Commit

---
+++ SwiiftUI_VerticalSlider/SwiiftUI_VerticalSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiiftUI_VerticalSliderApp.swift
+//  SwiiftUI_VerticalSlider
+//
+//  Created by park kyung seok on 2022/09/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiiftUI_VerticalSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
